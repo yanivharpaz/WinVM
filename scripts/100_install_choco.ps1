ccc81280e99b29b249e6fa5a6d3bbd5c9118bb20
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2021-09-28 10:54:27 +0300

    git clone and copy

--- scripts/100_install_choco.ps1
+++ scripts/100_install_choco.ps1
@@ -1,4 +1,3 @@
-
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -11,25 +10,27 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 # create directory for the powershell profile
 start cmd "/c mkdir %USERPROFILE%\Documents\PowerShell"
 
-choco install -y wget
+choco install -y wget 
+choco install -y git --params="'/GitAndUnixToolsOnPath /NoAutoCrlf'" --install-arguments="'/DIR=C:\swtools\git'"
+
+# reload environment
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+mkdir \dev
+cd \dev
+git clone https://github.com/yanivharpaz/WinVM.git
 
 # download font and theme to the desktop
 cd $env:USERPROFILE\Desktop
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/DelugiaCodeNerdFontComplete.ttf"
-start cmd "/c wget --no-check-certificate wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/yaniv_dark_01.deskthemepack"
+
+copy c:\dev\WinVM\setup_files\DelugiaCodeNerdFontComplete.ttf .
+copy c:\dev\WinVM\setup_files\yaniv_dark_01.deskthemepack .
 
 mkdir config
 cd config
 
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/settings.json"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/wincmd.ini"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/010_ext_vscode.cmd"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/omp.ps1"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/yh-requirements.txt"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/Microsoft.PowerShell_profile.ps1"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/030_python_first_env.cmd"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/001_setup_win_tools.cmd"
-start cmd "/c wget --no-check-certificate https://gitlab.com/yaniv.harpaz/notes/-/raw/master/older_stuff/020_python_ds_pkg.cmd"
+copy c:\dev\WinVM\setup_files\*.* .
+copy c:\dev\WinVM\scripts\*.* .
 
 choco install -y googlechrome
 
