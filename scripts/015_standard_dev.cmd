1d49e5d8a1ece86dc4f97c3977d40b90cf0a8b02
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2021-10-02 23:33:34 +0300

    added pycharm

--- scripts/015_standard_dev.cmd
+++ scripts/015_standard_dev.cmd
@@ -6,6 +6,8 @@ choco install -y docker-desktop
 
 choco install -y visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --includeOptional --passive --locale en-US"
 
+choco install -y pycharm-community
+
 choco install -y ssms
 
 start chrome aka.ms/buildtools
