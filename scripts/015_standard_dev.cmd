857adaacdcb2cb3b01cd45d687e9b987be66effd
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2021-10-06 20:53:05 +0300

    added docker-compose and shap

--- scripts/015_standard_dev.cmd
+++ scripts/015_standard_dev.cmd
@@ -3,6 +3,7 @@ choco install -y anaconda3
 choco install -y vcredist-all
 
 choco install -y docker-desktop
+choco install -y docker-compose
 
 choco install -y visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --includeOptional --passive --locale en-US"
 
