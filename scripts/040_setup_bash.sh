85a773035420f5387eb1f7d9992943adca0e70b4
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-05-21 18:09:09 +0300

    pre-merge

--- scripts/040_setup_bash.sh
+++ scripts/040_setup_bash.sh
@@ -16,15 +16,4 @@ sudo service docker restart
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
 
-sudo chsh -s `which zsh`
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
-sudo sed -i 's/plugins=(git)/plugins=(git git-flow brew history node npm kubectl zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
-
 
---
+++ scripts/050_setup_zsh_omz.sh
@@ -0,0 +1,11 @@
+
+sudo chsh -s `which zsh`
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+sudo sed -i 's/plugins=(git)/plugins=(git git-flow brew history node npm kubectl zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
