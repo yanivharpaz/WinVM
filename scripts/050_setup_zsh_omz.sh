54f81066beb02741472b2ff4c8096794eeaf1ad5
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-05-21 19:18:27 +0300

    pre-merge

--- scripts/050_setup_zsh_omz.sh
+++ scripts/050_setup_zsh_omz.sh
@@ -1,5 +1,7 @@
 sudo chsh -s `which zsh`
 
+cd ~
+
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
